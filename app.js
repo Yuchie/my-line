b1ebd76aec222e368901f012cc854db0c071d084
Yuchie <yuchie.gomi@gmail.com>
2017-05-15 19:17:05 +0900

    change app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 3000));
+
 app.get('/', function(req, res){
 	res.send('Hello World!');
 });
 
-var server = app.listen(process.env.PORT || 3000, "127.0.0.1", function(){
-	var host = server.address().address;
+var server = app.listen(app.get('port'), function(){
 	var port = server.address().port;
-	console.log('Example app listening at http;//' + host + ':' + port);
+	console.log('Example app listening at port:' + port);
 });
