23da0f1a7fd379d1ff7829a5a10dab68162cbf52
Yuchie <yuchie.gomi@gmail.com>
2017-05-15 18:49:41 +0900

    change into dynamic port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', function(req, res){
 	res.send('Hello World!');
 });
 
-var server = app.listen(3000, "127.0.0.1", function(){
+var server = app.listen(process.env.PORT || 3000, "127.0.0.1", function(){
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log('Example app listening at http;//' + host + ':' + port);
